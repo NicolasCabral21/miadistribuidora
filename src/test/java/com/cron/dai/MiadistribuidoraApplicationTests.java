1ae02cdaf28a33ba1a520bb8a2b1e548be3f2896
Nicolas Cabral <nicolas.cabral@localpayment.com>
2021-03-28 16:58:15 -0300

    se vuelven atras los archivos de mia distribuidora

---
+++ src/test/java/com/cron/dai/MiadistribuidoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cron.dai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiadistribuidoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
